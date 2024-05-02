668cefcc214a928826bbcc6765ac0de865e306df
Ignacio Avila <ignacioavilamdp@gmail.com>
2024-05-02 09:25:29 -0300

    pass 3th stage

--- app/main.py
+++ app/main.py
@@ -1,35 +1,46 @@
 # Uncomment this to pass the first stage
+import os
 import socket
 
 CRLF = '\r\n'
 
+HOST = "localhost"
+PORT = 4221
+
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+    # Run the server
+    server_socket = socket.create_server((HOST, PORT), reuse_port=False)
+    print(f"Server running and accepting connections at {HOST}:{PORT}{os.linesep}")
 
-    # Uncomment this to pass the first stage
-    #
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=False)
-    (connection_socket, address) = server_socket.accept()  # wait for client
+    (connection, address) = server_socket.accept()  # wait for client
+    print(f'Connection stablished with {address[0]}:{address[1]}{os.linesep}')
 
-    print(f'server_socket object: {connection_socket}')
-    print(f'address: {address}')
-    print()
+    # Recieve the request
+    request = connection.recv(1024).decode('utf-8')
 
-    status_line = 'HTTP/1.1 200 OK'
+    # Parse the request
+    request_line = request.split(CRLF)[0]
+    request_line_elements = request_line.split(' ')
+    http_method = request_line_elements[0]
+    request_target = request_line_elements[1]
+    http_version = request_line_elements[2]
+
+    # Response
+    if request_target == '/':
+        status_line = 'HTTP/1.1 200 OK'
+    else:
+        status_line = 'HTTP/1.1 400 Not Found'
     response = f'{status_line}{CRLF}{CRLF}'
 
-    if connection_socket:
-        message_bytes = connection_socket.recv(999)
-        message_text = message_bytes.decode('utf-8')
-        print('received message')
-        print(message_text)
-        print()
+    connection.send(response.encode('utf-8'))
 
-        print('message to send')
-        print(response)
-        connection_socket.send(response.encode('utf-8'))
+    # Debugging
+    print(f'Request line - http_method: {http_method}')
+    print(f'Request line - request_target: {request_target}')
+    print(f'Request line - http_version: {http_version}')
+    print(f'Complete request: {os.linesep}{request}')
+    print(f'Response: {os.linesep}{response}')
 
 
 if __name__ == "__main__":
