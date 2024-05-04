7722ece8395a725bd6c8c80c8cc83a090345745d
Ignacio Avila <ignacioavilamdp@gmail.com>
2024-05-04 14:40:15 -0300

    pass 5th stage

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ import string
 from enum import Enum
 
 CRLF = '\r\n'
+HTTP11 = 'HTTP/1.1'
 
 HOST = "localhost"
 PORT = 4221
@@ -24,64 +25,106 @@ class ResponseStatus(Enum):
 
 class HttpRequest:
 
-    def __init__(self, http_method, request_target, http_version, headers, body):
+    def __init__(self, http_method, request_target, http_version, headers_dict, body):
         self.http_method = http_method
         self.request_target = request_target
         self.http_version = http_version
-        self.headers = headers
+        self.headers_dict = headers_dict
         self.body = body
 
+    def __str__(self):
+        request_line = f'{self.http_method}, {self.request_target}, {self.http_version}'
+        headers_list = [f'{key}: {value}' for (key, value) in self.headers_dict.items()]
+        return CRLF.join([request_line, CRLF.join(headers_list), '', self.body])
+
     @staticmethod
-    def from_string(request_string):
-        request_line, *headers, empty_line, body = request_string.split(CRLF)
+    def from_string(request_string: str):
+        request_line, *headers_list, empty_line, body = request_string.split(CRLF)
         http_method, request_target, http_version = request_line.split(' ')
-        return HttpRequest(http_method, request_target, http_version, headers, body)
+
+        headers_dict = dict()
+        for header in headers_list:
+            key, sep, value = header.partition(':')
+            headers_dict[key] = value.strip()
+
+        return HttpRequest(http_method, request_target, http_version, headers_dict, body)
+
+    @staticmethod
+    def from_bytes(request_bytes:bytes):
+        return HttpRequest.from_string(request_bytes.decode(encoding='utf-8'))
 
 
 class HttpResponse:
-    def __init__(self, http_version, response_status: ResponseStatus, headers, body):
+    def __init__(self, http_version, response_status: ResponseStatus, headers_dict, body):
         self.http_version = http_version
         self.response_status = response_status
-        self.headers = headers
+        self.headers_dict = headers_dict
         self.body = body
 
-    def to_string(self):
+    def __str__(self):
         status_line = f'{self.http_version} {self.response_status.status_code} {self.response_status.status_text}'
-        return CRLF.join([status_line, CRLF.join(self.headers), '', self.body])
+        headers_list = [f'{key}: {value}' for (key, value) in self.headers_dict.items()]
+        return CRLF.join([status_line, CRLF.join(headers_list), '', self.body])
+
+    def to_bytes(self):
+        return str(self).encode('utf-8')
+
+
+def process_connection(connection_socket: socket):
+
+    with connection_socket:
+        # Fetch data and create the HttpRequest
+        data = connection_socket.recv(1024)
+        http_request = HttpRequest.from_bytes(data)
+
+        # Construct the response
+        if http_request.request_target.startswith('/echo/'):
+
+            body = http_request.request_target.removeprefix('/echo/')
+            headers = {'Content-Type': 'text/plain', 'Content-Length': str(len(body))}
+            http_response = HttpResponse(HTTP11, ResponseStatus.OK, headers, body)
+
+        elif http_request.request_target == '/user-agent':
+
+            body = http_request.headers_dict['User-Agent']
+            headers = {'Content-Type': 'text/plain', 'Content-Length': str(len(body))}
+            http_response = HttpResponse(HTTP11, ResponseStatus.OK, headers, body)
+
+        elif http_request.request_target == '/':
+            http_response = HttpResponse(HTTP11, ResponseStatus.OK, {}, '')
+
+        else:
+            http_response = HttpResponse(HTTP11, ResponseStatus.NOT_FOUND, {}, '')
+
+        # Send response
+        connection_socket.send(http_response.to_bytes())
+
+        # Debugging
+        print(f'Request line - http_method: {http_request.http_method}')
+        print(f'Request line - request_target: {http_request.request_target}')
+        print(f'Request line - http_version: {http_request.http_version}{os.linesep}')
+        print(f'Complete request: {os.linesep}{http_request}')
+        print(f'Response: {os.linesep}{http_response}')
+
+
+def run_server():
+    with socket.create_server((HOST, PORT), reuse_port=False) as server_socket:
+        print(f"Server running and accepting connections at {HOST}:{PORT}{os.linesep}")
+
+        # Main loop for incoming connections
+        while True:
+            (connection_socket, connection_adress) = server_socket.accept()  # wait for client connection
+            print(f'Connection stablished with {connection_adress[0]}:{connection_adress[1]}{os.linesep}')
+            process_connection(connection_socket)
 
 
 def main():
-    # Run the server
-    server_socket = socket.create_server((HOST, PORT), reuse_port=False)
-    print(f"Server running and accepting connections at {HOST}:{PORT}{os.linesep}")
+    try:
+        run_server()
+    except KeyboardInterrupt:
+        print('Server stopped with keyboard interrupt')
 
-    (connection, address) = server_socket.accept()  # wait for client
-    print(f'Connection stablished with {address[0]}:{address[1]}{os.linesep}')
-
-    # Receive and process the request
-    request_string = connection.recv(1024).decode('utf-8')
-    http_request = HttpRequest.from_string(request_string)
-
-    # Construct the response
-    if http_request.request_target.startswith('/echo/'):
-        body = http_request.request_target.removeprefix('/echo/')
-        content_type_header = 'Content-Type: text/plain'
-        content_length = f'Content-Length: {len(body)}'
-        http_response = HttpResponse('HTTP/1.1', ResponseStatus.OK, [content_type_header, content_length], body)
-    elif http_request.request_target == '/':
-        http_response = HttpResponse('HTTP/1.1', ResponseStatus.OK, [], '')
-    else:
-        http_response = HttpResponse('HTTP/1.1', ResponseStatus.NOT_FOUND, [], '')
-
-    # Send response
-    connection.send(http_response.to_string().encode('utf-8'))
-
-    # Debugging
-    print(f'Request line - http_method: {http_request.http_method}')
-    print(f'Request line - request_target: {http_request.request_target}')
-    print(f'Request line - http_version: {http_request.http_version}{os.linesep}')
-    print(f'Complete request: {os.linesep}{request_string}')
-    print(f'Response: {os.linesep}{http_response.to_string()}')
+    print('--------------------Server shutting down--------------------')
 
 
 if __name__ == "__main__":
